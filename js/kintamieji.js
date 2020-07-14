ee30f0243cf059a02318f644a7b41f5beedfb961
Deivis151 <jurkusdeividas64@gmail.com>
2020-07-14 13:57:53 +0300

    #1

---
+++ js/kintamieji.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const a = 6;
+const b = 7;
+const c = 12;
+
+console.log(a, b, c);
+
+const s1 = 'kase';
+const s2 = 'fule';
+const sportas = 'kase' + 'fule';
+
+console.log(sportas);
+
+const d = 19;
+const e = 29;
+const f = 49;
+const g = 59;
+const h = d + e + f + g;
+
+console.log(d, e, f, g, h);
+
+const k1 = 'automobilis'
+const k2 = 'motociklas'
+const k3 = 'autobusas'
+const k4 = 'traukinys'
+const priemones = k1 + k2 + k3 + k4;
+
+console.log(priemones);
+
+const skaiciai = [12, 29, 49, 59, 19];
+
+console.log(skaiciai);
+
+const rezultatas = 12 + 29 + 49 + 59 + 19;
+
+console.log(rezultatas);
+
+const lang = 'en';
+const good = {
+    en: 'Good',
+
+};
+const morning = {
+    en: 'morning'
+};
+const goodMorning = good[lang] + ' ' + morning[lang] + '!';
+
+console.log(goodMorning)
+
+const sample = 1 - 2 + 3 - 4 + 5
+console.log(sample);
